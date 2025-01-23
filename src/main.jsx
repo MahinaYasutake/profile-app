579b1dccca8534240216efab8217ebcd7a594d1f
Mahina Yasutake <mahina.yasutake@gmail.com>
2025-01-22 22:28:18 -0500

    edited main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './components/Header';
@@ -9,8 +10,8 @@ import Card from './components/Card';
 
 export default App;
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
