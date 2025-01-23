cd8cb717bda3128b6e2c36d15efb524adba67cf0
Mahina Yasutake <mahina.yasutake@gmail.com>
2025-01-23 17:38:26 -0500

    changed the base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CGT 390 - MAHINA Y/',
+  base: '/profile-app/',
   },
 );
